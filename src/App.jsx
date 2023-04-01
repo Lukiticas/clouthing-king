489f3b1afe7c091daf01c411574282b22610ec09
Lucas Matheus Nunes Pereira <Lucasmathues416@outlook.com>
2023-04-01 12:25:44 -0300

    fux

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,13 @@ import { Route, Routes } from "react-router-dom";
 import Home from "./routes/home/Home.component";
 import Nav from "./routes/nav/Nav.component";
 import SignPage from "./routes/signPage/SignPage.component";
-
+import Shop from "./routes/shop/Shop.component";
 const App = () => {
   return (
     <Routes>
       <Route path="/" element={<Nav />}>
         <Route index element={<Home />} />
+        <Route path="shop" element={<Shop />} />
         <Route path="signIn" element={<SignPage />} />
       </Route>
     </Routes>
--- src/main.jsx
+++ src/main.jsx
@@ -5,14 +5,17 @@ import "./styles/global.scss";
 
 import { BrowserRouter } from "react-router-dom";
 import { UserProvider } from "./contexts/user.context";
+import { ProductsProvider } from "./contexts/products.context";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <UserProvider>
-        <App />
-      </UserProvider>
+      <ProductsProvider>
+        <UserProvider>
+          <App />
+        </UserProvider>
+      </ProductsProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
